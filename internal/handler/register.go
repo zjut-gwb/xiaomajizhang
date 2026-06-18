ebb879eb8513daba0140e500fb7aea57c7489669
asikeida <asikeida@gmail.com>
2026-06-18 23:18:48 +0800

    init

--- internal/handler/register.go
+++ internal/handler/register.go
@@ -1,1 +1,7 @@
 package handler
+
+type RegisterRequest struct {
+	Username string `json:"username" binding:"required,min=3,max=50"`
+	Email    string `json:"email" binding:"required,email,max=100"`
+	Password string `json:"password" binding:"required,min=8,max=72"`
+}
